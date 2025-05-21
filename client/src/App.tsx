ab5b0f3d8300fff2dbccde0bed3a2871abf10fc2
Nick Yang <nick.yang@activenetwork.com>
2025-05-21 11:12:23 +0800

    change the project client to a typescript project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { JSX, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
