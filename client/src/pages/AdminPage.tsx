553a61f81d31fa5a8fb0fb53e7806388f28b1487
Nick Yang <nick.yang@activenetwork.com>
2025-05-21 15:44:26 +0800

    add client side page routing

---
+++ client/src/pages/AdminPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Admin Page</h2>
+      <p>Welcome to the Admin Page!</p>
+    </div>
+  );
+};
+
+export default AdminPage;
