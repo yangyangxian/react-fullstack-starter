4287e0ff896cb1e47fb99d7ebae76812c858e52d
Yang Yang <persistence@vip.qq.com>
2025-06-04 22:10:43 +0800

    fixed a env file loading issue when running app from root dir

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,15 +10,19 @@ const GetEnv = (key: string, fallback?: string): string => {
   throw new Error(`Missing required env var: ${key}`);
 };
 
-console.log('NODE_ENV (before dotenv.config()):', process.env.NODE_ENV);
 // Load default .env first
 dotenv.config({ path: '.env' });
 // Load env-specific file if it exists
 const envFile = `.env.${GetEnv('NODE_ENV', 'development')}`;
+const envFile2 = `server/.env.${GetEnv('NODE_ENV', 'development')}`;   //this monorepo will run from the root dir
 if (fs.existsSync(envFile)) {
-  dotenv.config({ path: envFile });
+  dotenv.config({ path: envFile , override: true });
+} else if (fs.existsSync(envFile2)) {
+  dotenv.config({ path: envFile2 , override: true });
+} else {
+  console.log(`Environment file ${envFile} not found`);
 }
-console.log('NODE_ENV (after dotenv.config()):', process.env.NODE_ENV);
+
 const configs = {
   port: parseInt(GetEnv('PORT', '5050'), 10),
   dbUrl: GetEnv('DATABASE_URL', 'mongodb://localhost:27017/my_database'),
